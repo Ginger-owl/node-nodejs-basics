4af9228ba2eb312616adb47864abd196d3c60fad
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-06-20 02:27:25 +0400

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    for (let variable in process.env) {
+      if (/^RSS_*/.test(variable)) {
+        console.log(process.env[variable])
+      }
+    }
 };
 
 parseEnv();
