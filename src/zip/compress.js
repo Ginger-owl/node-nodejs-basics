1085fc80d8d82306426b0fb0f41f5db3c3d41409
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-06-20 03:34:47 +0400

    feat: implement compress.js and decompress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
-const compress = async () => {
-    // Write your code here 
+import {resolve, dirname} from 'path';
+import { createReadStream, createWriteStream} from 'fs';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pathToFile = resolve(__dirname, './files/fileToCompress.txt');
+const pathToCompressedFile = resolve(__dirname, './files/archive.gz');
+
+const compress = async (file, compressedFile) => {
+  const gzip = createGzip();
+  const input = createReadStream(file);
+  const output = createWriteStream(compressedFile);
+  input.pipe(gzip).pipe(output);  
 };
 
-await compress();
+await compress(pathToFile, pathToCompressedFile);
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
-const decompress = async () => {
-    // Write your code here 
+import {resolve, dirname} from 'path';
+import { createReadStream, createWriteStream} from 'fs';
+import { fileURLToPath } from 'url';
+import { createUnzip } from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pathToCompressedFile = resolve(__dirname, './files/archive.gz');
+const pathToFile = resolve(__dirname, './files/fileToCompress.txt');
+
+const decompress = async (compressedFile, file) => {
+  const unzip = createUnzip();
+  const input = createReadStream(compressedFile);
+  const output = createWriteStream(file);
+  input.pipe(unzip).pipe(output);
 };
 
-await decompress();
+await decompress(pathToCompressedFile, pathToFile);
