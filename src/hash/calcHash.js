abd81c3586494c32ee842327f00314d19cfbf113
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-06-20 03:05:46 +0400

    feat: implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { resolve, dirname } from 'path';
+import { readFile } from 'fs';
+import { fileURLToPath } from 'url';
+import { createHash } from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pathToFile = resolve(__dirname, './files/fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  readFile(pathToFile, 'utf8', (err, data) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    }
+    const string = data;
+
+    const hashRes = createHash('sha256').update(string).digest('hex');
+    console.log(hashRes)
+  });
 };
 
-await calculateHash();
+await calculateHash(pathToFile);
