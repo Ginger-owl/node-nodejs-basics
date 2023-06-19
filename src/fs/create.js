fa8b20284f6bfeda13054fa468059bcd7ba715ae
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-06-20 01:02:14 +0400

    feat: add code to create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { access, F_OK, writeFileSync } from 'fs';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const pathToFile = path.resolve('./files/fresh.txt');
+
+  // try access path to file
+  access(pathToFile, F_OK, (e) => {
+    // if file is not found - create fresh.txt
+    if (e) {
+      const content = 'I am fresh and young';
+      writeFileSync('./files/fresh.txt', content);
+      return
+    }
+    // else throw error
+    throw new Error('FS operation failed');
+  })
 };
 
 await create();
