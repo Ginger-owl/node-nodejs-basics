98d3a9db0870d83df64414fa8603bd05f6b65f25
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-06-20 01:59:29 +0400

    feat: implement read and rename

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
-const read = async () => {
-    // Write your code here 
+import { readFile } from 'fs';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pathToFile = resolve(__dirname, './files/fileToRead.txt');
+
+const read = async (pathToFile) => {
+    readFile(pathToFile, 'utf8', (err, data) => {
+      if (err) {
+        throw new Error('FS operation failed');
+      }
+      console.log(data);
+    });
 };
 
-await read();
+await read(pathToFile);
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,31 @@
-const rename = async () => {
-    // Write your code here 
+import { resolve, dirname } from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const wrongFilePath = resolve(__dirname, './files/wrongFilename.txt')
+const rightFilePath = resolve(__dirname, './files/properFilename.md') 
+
+
+const rename = async (filePath, copyPath) => {
+  fs.access(copyPath, err => {
+    if (!err) {
+      throw new Error('FS operation failed');
+    }
+  })
+  fs.access(filePath , err => {
+    if (err) {
+      throw new Error('FS operation failed');
+    }
+  })
+
+    fs.rename(filePath, copyPath, (err) => {
+      if (err) {
+        throw new Error('FS operation failed');
+      }
+  });
 };
 
-await rename();
+await rename(rightFilePath, wrongFilePath);
