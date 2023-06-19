f5b8687814bbe4a559d168088224b2b4845602e2
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-06-20 03:56:09 +0400

    feat: implement workers.js and main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
-const performCalculations = async () => {
-    // Write your code here
+import { cpus }from 'os';
+import { Worker } from 'worker_threads';
+import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pathToWorker = resolve(__filename, '../worker.js')
+
+
+const performCalculations = async (pathToWorker) => {
+  
+  const systemCpuCores = cpus();
+
+  for (let i= 0; i < systemCpuCores.length; i++ ) {
+    const worker = new Worker(pathToWorker, {
+      workerData: {
+        value: 10 + i,
+        path: pathToWorker
+      }
+    });
+     
+    worker.on('message', (result) => {
+      console.log(result);
+    });
+  }
+  
 };
 
-await performCalculations();
+await performCalculations(pathToWorker);
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,7 +1,12 @@
+import { parentPort, workerData } from 'worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
+  return parentPort.postMessage(
+    nthFibonacci(workerData.value)
+  );
     // This function sends result of nthFibonacci computations to main thread
 };
 
