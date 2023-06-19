00be757c6de957609195f81ab2def0f7e88bc220
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-06-20 01:45:59 +0400

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,34 @@
-const list = async () => {
-    // Write your code here 
+import { resolve, dirname } from 'path';
+import { readdir, stat } from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const folderPath = resolve(__dirname, 'files')
+
+const list = async (folderPath) => {
+  readdir(folderPath,
+    (err, files) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      files.forEach(file => {
+        stat(resolve(folderPath, file), (err, stats) => {
+          if (err) {
+            throw new Error('FS operation failed');
+          } else {
+            if (stats.isFile()) {
+              const fileName = file.split('.')[0];
+              console.log(`${fileName}`);
+            }
+          }
+        });
+      })
+    }
+  });
 };
 
-await list();
+
+
+await list(folderPath);
