194df1a588a9dbc3d0feb8134ea2059ec46df41f
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-06-20 03:23:22 +0400

    feat: implement read.js and write.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
-const read = async () => {
-    // Write your code here 
+import {resolve, dirname} from 'path';
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pathToFile = resolve(__dirname, './files/fileToRead.txt');
+
+const read = async (pathToFile) => {
+  const readStream  = createReadStream(pathToFile);
+  readStream.on('data', (chunk) => {
+    process.stdout.write(chunk);
+ });
 };
 
-await read();
+await read(pathToFile);
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
-const write = async () => {
-    // Write your code here 
+import {resolve, dirname} from 'path';
+import { createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pathToFile = resolve(__dirname, './files/fileToWrite.txt');
+
+
+const write = async pathToFile => {
+  const writableStream = createWriteStream(pathToFile);
+
+  for await (const chunk of process.stdin) {
+    writableStream.write(chunk);
+  }
 };
 
-await write();
+await write(pathToFile)
