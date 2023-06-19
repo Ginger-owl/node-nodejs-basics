9b798a713f0f187acf714851873343167f8231f1
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-06-20 01:37:55 +0400

    feat: implemented delete.js, tinker create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,19 +1,24 @@
 import { access, F_OK, writeFileSync } from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const create = async () => {
-  const pathToFile = path.resolve('./files/fresh.txt');
+  const pathToFile = path.resolve(__dirname, './files/fresh.txt');
 
   // try access path to file
   access(pathToFile, F_OK, (e) => {
     // if file is not found - create fresh.txt
     if (e) {
       const content = 'I am fresh and young';
-      writeFileSync('./files/fresh.txt', content);
+      writeFileSync(pathToFile, content);
       return
+    } else {
+      throw new Error('FS operation failed');
     }
     // else throw error
-    throw new Error('FS operation failed');
   })
 };
 
--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { resolve, dirname } from 'path';
+import { unlink } from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pathToFile = resolve(__dirname, './files/fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+  unlink(pathToFile, err => {
+    if (err) {
+      throw new Error('FS operation failed');
+    }
+  })
 };
 
 await remove();
