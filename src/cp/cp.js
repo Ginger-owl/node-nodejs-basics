4f41d7773d906e39c3674e350edeb6833c852497
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-06-20 05:08:53 +0400

    feat: implement cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fork } from 'child_process';
+import {resolve, dirname} from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pathToChild= resolve(__dirname, './files/script.js');
+
+const spawnChildProcess = async (...args) => {
+  const child = fork(pathToChild, args);
+
+  child.on("error", (err) => {
+    console.log(err);
+  });
+
+  child.on("close", (code) => {
+    console.log("child process exited with code " + code);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/* 1, 2, 'abc', false */);
