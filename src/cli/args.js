acb8e91fcc0ea1e0465c71c063117138b17dce32
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-06-23 04:18:52 +0400

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,9 +1,14 @@
 const parseArgs = () => {
-    const argsArray = [];
-    for (let name in process.env) {
-      argsArray.push(`${name} is ${process.env[name]}`)
-    }
-    process.stdout.write(argsArray.join(', '))
+  const args = process.argv.slice(2)
+  const argsArray = []
+
+  for (let i = 0; i < args.length; i += 2) {
+    const propName = args[i].slice(2)
+    const propValue = args[i + 1]
+    argsArray.push(`${propName} is ${propValue}`)
+  }
+  
+  process.stdout.write(argsArray.join(', '))
 };
 
 parseArgs();
