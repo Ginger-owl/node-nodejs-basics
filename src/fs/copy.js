cdaeef9b7a98dcacfb2924d8bb9f887305f4e13e
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-06-23 05:38:22 +0400

    refactor: fix and refactor copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,45 +1,25 @@
 import path from 'path';
-import fs from 'fs';
+import { access, cp }  from 'fs';
 import { fileURLToPath } from 'url';
-import { mkdir, readdir, copyFile } from 'fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const copy = async () => {
-  const folderPath = path.resolve(__dirname, 'files');
-  const copyFolderPath = path.resolve(__dirname, 'files-copy');
-    // Write your code here 
-  const copyFolder = async (folderPath, CopyPath) => {
-    fs.access(folderPath, err => {
-      if (err) {
-        throw new Error('FS operation failed');
-      }
-    })
-    
-    fs.access(CopyPath, err => {
-      if (!err) {
-        throw new Error('FS operation failed');
-      } else {
-        mkdir(CopyPath, { recursive: true }, err => {
-          if (err) {
-            throw new Error(err)
-          }
-        })
-      }
-    })
-    
-    readdir(folderPath, { withFileTypes: true})
-    .then(files => files.forEach(file => {
-      if (file.isDirectory()) {
-        copyFolder(path.resolve(folderPath, file.name), path.resolve(CopyPath, file.name))
-      } 
-      if (file.isFile()) {
-        copyFile(path.resolve(folderPath, file.name), path.resolve(CopyPath, file.name))
-      }
-    }))
-  }
-  copyFolder(folderPath, copyFolderPath)
+const srcPath = path.resolve(__dirname, 'files');
+const destPath = path.resolve(__dirname, 'files_copy');
+
+const copy = async (srcPath, destPath) => {
+  access(destPath, err => {
+    if (!err) {
+      throw new Error('FS operation failed');
+    } 
+  })
+  
+  cp(srcPath, destPath, { recursive: true }, (err) => {
+    if (err) {
+      throw new Error('FS operation failed'); 
+    }
+  });
 };
 
-await copy();
+await copy(srcPath, destPath);
